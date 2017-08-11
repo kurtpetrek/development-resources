0af87bf384aad522a74351a4e24640e139d85bdd
Kurt Petrek <kurtpetrek@gmail.com>
2017-08-11 19:30:13 -0400

    building out content object and functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,221 @@
+var articleData = {
+  references: [
+    {
+      title: 'W3Schools',
+      description: 'W3Schools is a great website to reference as you are starting to learn web development.',
+      linkURL: 'http://www.w3schools.com'
+    },
+    {
+      title: 'Mozilla Developer Network',
+      description: 'Mozilla Developer Network is the authority resource for in depth and accurate information on web development, it can be less beginner friendly than W3Schools but goes much more in depth with information.',
+      linkURL: 'https://developer.mozilla.org/en-US/'
+    },
+    {
+      title: 'Stack Overflow',
+      description: 'Stack Overflow is where you can find answers to questions in a lot of different coding languages and ask your own questions.',
+      linkURL: 'https://stackoverflow.com/'
+    },
+    {
+      title: 'CSS Tricks',
+      description: 'Tips, Tricks, and Techniques on using Cascading Style Sheets and much much more.',
+      linkURL: 'https://css-tricks.com/'
+    },
+    {
+      title: 'Command Line Cheat Sheet',
+      description: 'This is a cheat sheet for command line (unix terminal) commands and a brief description of what they do.',
+      linkURL: 'https://www.git-tower.com/blog/command-line-cheat-sheet/'
+    },
+    {
+      title: 'Git Cheat Sheet',
+      description: 'This is a cheat sheet for git commands with a brief description of what they do.',
+      linkURL: 'https://www.git-tower.com/blog/git-cheat-sheet/'
+    },
+    {
+      title: 'Markdown Cheat Sheet',
+      description: 'This is a cheat sheet for writing Markdown.',
+      linkURL: 'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet'
+    },
+    {
+      title: 'Can I Use',
+      description: 'Not all web browsers have the same functionality and features, this website will help show you what works in different versions of the most widely used web browsers.',
+      linkURL: 'http://caniuse.com/'
+    },
+    {
+      title: 'Can I Use',
+      description: 'Not all web browsers have the same functionality and features, this website will help show you what works in different versions of the most widely used web browsers.',
+      linkURL: 'http://caniuse.com/'
+    }
+  ],
+  tutorials: [
+    {
+      title: 'Codecademy',
+      description: 'Codecademy has a variety of lessons to help you learn HTML, CSS, and JavaScript as well as other programming languages.',
+      linkURL: 'https://www.codecademy.com/'
+    },
+    {
+      title: 'Free Code Camp',
+      description: 'Free Code Camp has some great lessons to help you learn HTML, CSS, and JavaScript.',
+      linkURL: 'https://www.freecodecamp.com/'
+    },
+    {
+      title: 'Khan Academy',
+      description: 'Khan Academy may be meant for younger audiences but it breaks things down and explains things in a simple manner that can be easy to follow along with.',
+      linkURL: 'https://www.khanacademy.org/computing'
+    },
+    {
+      title: 'Markdown Tutorial',
+      description: 'Markdown is a way to write content for the web. Websites like GitHub and reddit use Markdown to style their comments. This is a tutorial to help learn Markdown.',
+      linkURL: 'http://www.markdowntutorial.com/'
+    },
+    {
+      title: 'Learning Git Branching',
+      description: 'Learning Git Branching is an interactive visual tutorial to help you learn Git.',
+      linkURL: 'http://learngitbranching.js.org/'
+    },
+    {
+      title: 'RegExr',
+      description: 'A website to help learn regular expressions.',
+      linkURL: 'http://regexr.com/'
+    }
+  ],
+  tutorialVideos: [
+    {
+      title: 'Lynda.com',
+      description: 'Lynda offers a huge range of tutorial videos on a lot of different software and development languages. Many public libraries across the nation provide free membership to lynda with a library card. To access Lynda.com with a New York Public Library card visit <a href="https://www.nypl.org/lynda" target="_blank">nypl.org/lynda</a> and click the connect to database link. There are also many free courses you can take at the New York Public Library, checkout <a href="https://www.nypl.org/computers" target="_blank">nypl.org/computers</a> for more information.',
+      linkURL: 'https://www.lynda.com/'
+    },
+    {
+      title: 'DevTips',
+      description: 'A youtube channel for web development focusing on design.',
+      linkURL: 'https://www.youtube.com/user/DevTipsForDesigners'
+    },
+    {
+      title: 'Udacity',
+      description: 'Udacity offers some free online courses that provide some great information.',
+      linkURL: 'https://www.udacity.com'
+    },
+    {
+      title: 'Egghead',
+      description: 'Egghead has some great free videos, though some may be a bit advanced for beginners they provide great information.',
+      linkURL: 'https://egghead.io/'
+    },
+    {
+      title: 'FreeCodeCamp',
+      description: 'Free Code Camps youtube channel offers a lot of great videos on web development.',
+      linkURL: 'https://www.youtube.com/channel/UC8butISFwT-Wl7EV0hUK0BQ'
+    }
+  ],
+  images: [
+    {
+      title: 'Pixabay',
+      description: 'A site that offers free images that can be used for commercial and non-commercial projects.',
+      linkURL: 'https://pixabay.com/'
+    },
+    {
+      title: 'Pexels',
+      description: 'Another site for free images that can be used for commercial and non-commercial projects.',
+      linkURL: 'https://www.pexels.com/'
+    },
+    {
+      title: 'Transparent Textures',
+      description: 'A site of transparent textures to be used with backgrounds.',
+      linkURL: 'https://www.transparenttextures.com/'
+    },
+    {
+      title: 'Subtle Patterns',
+      description: 'Another site of transparent textures to be used with backgrounds though attribution is required in a comment on CSS document.',
+      linkURL: 'https://www.toptal.com/designers/subtlepatterns/'
+    },
+    {
+      title: 'My Site My Way',
+      description: 'A site of free background images, textures, icons and more that can be used for commercial and non-commercial projects.',
+      linkURL: 'http://webtreats.mysitemyway.com/'
+    },
+    {
+      title: 'Pixlr Editor',
+      description: 'A free cloud based image editing and creation application for raster graphics that can be used directly within a web browser.',
+      linkURL: 'https://pixlr.com/editor/'
+    },
+    {
+      title: 'GIMP',
+      description: 'GIMP is a cross-platform image editor available for GNU/Linux, OS X, Windows and more operating systems. It is free software, you can change its source code and distribute your changes.',
+      linkURL: 'https://www.gimp.org/'
+    },
+    {
+      title: 'Inkscape',
+      description: 'Inkscape is a free piece of software for creating vector images. Inkscape is a free alternative to Adobe\'s Illustrator.',
+      linkURL: 'https://inkscape.org/en/'
+    }
+  ],
+  color: [
+    {
+      title: 'Coolors',
+      description: 'Colors are great, color schemes are awesome. Coolors will give you new color schemes with a tap of the space bar, bookmark the URL with the color scheme you like or create an account to save it for later.',
+      linkURL: 'https://coolors.co/'
+    },
+    {
+      title: 'Adobe Color',
+      description: 'Adobe Color can help you generate your own color scheme by using a color rule or you can explore color schemes others have created already. You do not need to have an Adobe CC membership to use this tool.',
+      linkURL: 'https://color.adobe.com/'
+    },
+    {
+      title: 'UI Gradients',
+      description: 'A site of gradients to help you build your UI.',
+      linkURL: 'https://uigradients.com/'
+    }
+  ]
+};
+
+function createArticleItem(item) {
+  var d = document.createElement('div');
+  var contentString = '<h3><a target="_blank" href="' +  item.linkURL + '"><h3>' + item.title + '</a></h3>';
+  contentString += '<p>' + item.description;
+  contentString += '</p><hr>';
+  // contentString += '<a target="_blank" href="' + item.linkURL + '">' + item.linkText + '</a>';
+  d.innerHTML = contentString;
+  return d;
+}
+
+function createArticleContent(){
+  var contentContainer = document.createElement('div');
+
+  contentContainer.innerHTML += '<h2 id="references">References</h2>';
+  articleData.references.forEach(function(x){
+    var item = createArticleItem(x);
+    contentContainer.appendChild(item);
+  });
+
+  contentContainer.innerHTML += '<h2 id="tutorials">Tutorials</h2>';
+  articleData.tutorials.forEach(function(x){
+    var item = createArticleItem(x);
+    contentContainer.appendChild(item);
+  });
+
+  contentContainer.innerHTML += '<h2 id="tutorial-videos">Tutorial Videos</h2>';
+  articleData.tutorialVideos.forEach(function(x){
+    var item = createArticleItem(x);
+    contentContainer.appendChild(item);
+  });
+
+  contentContainer.innerHTML += '<h2 id="images">Images</h2>';
+  articleData.images.forEach(function(x){
+    var item = createArticleItem(x);
+    contentContainer.appendChild(item);
+  });
+
+  contentContainer.innerHTML += '<h2 id="color">Color</h2>';
+  articleData.color.forEach(function(x){
+    var item = createArticleItem(x);
+    contentContainer.appendChild(item);
+  });
+
+  document.querySelector('.resource-items').innerHTML = '';
+  document.querySelector('.resource-items').appendChild(contentContainer);
+}
+
+createArticleContent();
+
+
 function toggleNav(){
   var ham = document.querySelector('#nav-toggle .hamburger');
   ham.classList.toggle('closed');
@@ -98,14 +316,6 @@ function codeCanvas() {
 
 codeCanvas();
 
-
-
-
-
-
-
-
-
 window.onresize = function (event) {
   if((window.innerHeight % 150 > 0 && window.innerHeight % 150 < 10) || (window.innerWidth % 50 > 0 && window.innerWidth % 50 < 10)){
     clearInterval(codeWorkAnm.interval);
@@ -115,18 +325,15 @@ window.onresize = function (event) {
 
 window.addEventListener('scroll', function (e) {
   var pos = window.pageYOffset;
-  var width = window.innerHeight || window.clientHeight;
+  var height = window.innerHeight || window.clientHeight;
 
-  if (pos < width) {
+  if (pos < height) {
     var html = document.querySelector('.landing__html');
     var css = document.querySelector('.landing__css');
     var js = document.querySelector('.landing__js');
     var php = document.querySelector('.landing__php');
     var ruby = document.querySelector('.landing__ruby');
-    var scale = 1;
-    // if (pos > 1 && Math.round((pos / 200) * 10) / 10 > 1) {
-    //   scale = Math.round((pos / 200) * 10) / 10;
-    // }
+
 
     html.style.transform = 'translate(' + Math.floor(pos / 8) + '%,' + Math.floor(pos / 5) + '%)';
     css.style.transform = 'translate(' + Math.floor(pos / -8) + '%,' + Math.floor(pos / -5) + '%)';
