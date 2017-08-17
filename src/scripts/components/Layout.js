4a281badd4ef9a61dde48889f8324832d85ce4db
Your Name <ian.sudderth@gmail.com>
2017-08-17 17:53:37 -0400

    adds eslint and prettier configs

--- src/scripts/components/Layout.js
+++ src/scripts/components/Layout.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
-import Nav from './Nav/Nav.js';
-import MainContent from './MainContent/MainContent.js';
+import React, { Component } from "react";
+import Nav from "./Nav/Nav.js";
+import MainContent from "./MainContent/MainContent.js";
 
 class Layout extends Component {
-
   render() {
     return (
       <div>
-        <Nav
-          navOpen={this.props.navOpen}
-          handleToggle={this.props.toggleNav}
-        />
+        <Nav navOpen={this.props.navOpen} handleToggle={this.props.toggleNav} />
         <MainContent
           navStatus={this.props.navOpen}
           articleData={this.props.articleData}
--- src/scripts/components/MainContent/MainContent.js
+++ src/scripts/components/MainContent/MainContent.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import Landing from './Landing.js';
-import Welcome from './Welcome.js';
-import ArticleList from './ArticleList.js';
+import React, { Component } from "react";
+import Landing from "./Landing.js";
+import Welcome from "./Welcome.js";
+import ArticleList from "./ArticleList.js";
 
 class MainContent extends Component {
-  render(){
-    let navStatus = '';
-    if(this.props.navStatus){
-      navStatus = 'main-content-moved';
+  render() {
+    let navStatus = "";
+    if (this.props.navStatus) {
+      navStatus = "main-content-moved";
     }
-    return(
+    return (
       <section id="main-content" className={navStatus}>
         <Landing />
         <Welcome />
         <ArticleList articleData={this.props.articleData} />
       </section>
-    )
+    );
   }
 }
 
