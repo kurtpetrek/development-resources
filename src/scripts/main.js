417bcbef4dadd8a19db6596fbaf42be6aa1126e7
Kurt Petrek <kurtpetrek@gmail.com>
2017-08-31 11:49:35 -0400

    Updates

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Zen from "zenscroll";
-
-import App from "./App";
-
-import "../styles/styles.scss";
-
-ReactDOM.render(<App />, document.getElementById("react-container"));
